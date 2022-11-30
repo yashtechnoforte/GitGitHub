752f848d1e5623966fd2bc90b068b6ada7fe7789
Yash <yash.mohan@technoforte.co.in>
2022-11-30 15:50:33 +0530

    initial commit

---
+++ Practice.java
@@ -0,0 +1,14 @@
+public class Practice{
+    public static void main(String[] args) {
+        System.out.println("Hello, I am Yash");
+        myFullName();
+        Practice age=new Practice();
+        age.myAge();
+    }
+    public static void myFullName(){
+        System.out.println("Yash S");
+    }
+    public void myAge(){
+        System.out.println("22");
+    }
+}
