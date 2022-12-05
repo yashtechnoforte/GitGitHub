5276a7cc6fdf062dfc25b80d48a12232365e45f6
Yash <yash.mohan@technoforte.co.in>
2022-12-05 11:28:44 +0530

    Updated Practice3.java

---
+++ Practice3.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Practice3 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        System.out.println(a+b);
+        Practice3 obj=new Practice3(4, 2);
+    }
+    public Practice3(int a,int b){
+        System.out.println(a-b);
+    }
+}
