f5db97acab1ce733e6bde7eb294c8e32626b2c7a
Yash <yash.mohan@technoforte.co.in>
2022-12-05 11:41:40 +0530

    Updated

--- Practice2.java
+++ Practice2.java
@@ -1,6 +1,10 @@
 public class Practice2 {
     public static void main(String[] args) {
-        System.out.println("Hi");
+        System.out.print("Hi");
         System.out.print(" There");
+        Practice2.m1();
+    }
+    public static void m1(){
+        System.out.println("XYZ");
     }
 }
