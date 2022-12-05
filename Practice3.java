12faca45a84079738cf323fdf8260b0e1eab52b0
Yash <yash.mohan@technoforte.co.in>
2022-12-05 18:14:42 +0530

    Updated

--- Practice3.java
+++ Practice3.java
@@ -6,12 +6,5 @@ public class Practice3 {
         int a=sc.nextInt();
         int b=sc.nextInt();
         System.out.println(a+b);
-        Practice3 obj=new Practice3(4, 2);
     }
-    public Practice3(int a,int b){
-        System.out.println(a-b);
-    }
-    // public Practice3(int a){
-    //     System.out.println(a+a);
-    // }
 }
