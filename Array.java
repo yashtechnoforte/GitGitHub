d23c361d298191aa65537ed352663ac27e30aef7
Yash <yash.mohan@technoforte.co.in>
2022-12-06 10:11:39 +0530

    Project Array.java added

---
+++ Array.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Array {
+    // public static void main(String[] args) {
+    //     //Scanner sc=new Scanner(System.in);
+    //     //int n=sc.nextInt();
+    //     //int input=sc.nextInt();
+    //     int[] array=new int[3];
+    //     System.out.println(array);
+    // }
+    public static void main(String[] args)
+        {
+            int n;
+            Scanner sc=new Scanner(System.in);
+            System.out.print("Enter Array Size:");
+            n=sc.nextInt();
+            int[] array = new int[n];
+            System.out.println("Enter Array elements: ");
+            for(int i=0; i<n; i++)
+            {
+                array[i]=sc.nextInt();
+            }
+    
+            // for (int i=0; i<n; i++)
+            // {
+            //     System.out.println(array[i]);
+            // }
+        }
+    
+}
